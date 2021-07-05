946da30895994e34551855f3ff1273f529de1652
KarolisGaiv <kgaivenis@gmail.com>
2021-07-05 15:04:17 +0300

    Add basic funcionality to print local weather data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log("test");
+const API_key = "7ad8fbf6434376408725f165c56e002d";
+let latitude;
+let longitude;
+
+function getLocation() {
+  if (window.navigator.geolocation) {
+    window.navigator.geolocation.getCurrentPosition(
+      successfulLookup,
+      console.log
+    );
+  }
+}
+
+function successfulLookup(position) {
+  latitude = position.coords.latitude;
+  longitude = position.coords.longitude;
+
+  getLocationWeather(latitude, longitude);
+}
+
+async function getLocationWeather(lat, lon) {
+  const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}`;
+
+  const response = await fetch(URL, { mode: "cors" });
+  const weatherData = await response.json();
+  console.log(weatherData);
+}
+
+getLocation();
